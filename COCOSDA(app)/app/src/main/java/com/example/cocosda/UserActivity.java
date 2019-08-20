c986867ea41773531be7e096a3dfc5aca9dedc92
goriko <guanzonedrian@gmail.com>
2019-08-20 17:02:44 +0800

    qr scanner

--- COCOSDA(app)/app/src/main/java/com/example/cocosda/UserActivity.java
+++ COCOSDA(app)/app/src/main/java/com/example/cocosda/UserActivity.java
@@ -4,12 +4,14 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -17,18 +19,25 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
-import org.w3c.dom.Text;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class UserActivity extends AppCompatActivity implements View.OnClickListener {
+public class UserActivity extends AppCompatActivity
+        implements View.OnClickListener{
 
-    private Button buttonLogout;
+    private Button buttonLogout, buttonScan;
     private TextView textViewName;
 
     private FirebaseAuth firebaseAuth;
     private DatabaseReference databaseReference;
 
     private ProgressDialog progressDialog;
+
+    private IntentIntegrator qrScan;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +51,10 @@ public class UserActivity extends AppCompatActivity implements View.OnClickListe
         progressDialog.show();
 
         buttonLogout = (Button) findViewById(R.id.buttonLogout);
+        buttonScan = (Button) findViewById(R.id.buttonScan);
         textViewName = (TextView) findViewById(R.id.textViewName);
+
+        qrScan = new IntentIntegrator(this);
 
         firebaseAuth = FirebaseAuth.getInstance();
         databaseReference = FirebaseDatabase.getInstance().getReference("users").child(firebaseAuth.getCurrentUser().getUid());
@@ -72,6 +84,7 @@ public class UserActivity extends AppCompatActivity implements View.OnClickListe
         });
 
         buttonLogout.setOnClickListener(this);
+        buttonScan.setOnClickListener(this);
     }
 
     @Override
@@ -79,6 +92,37 @@ public class UserActivity extends AppCompatActivity implements View.OnClickListe
         if(v == buttonLogout){
             firebaseAuth.signOut();
             startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        }else if(v == buttonScan){
+            qrScan.initiateScan();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            //if qrcode has nothing in it
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Result Not Found", Toast.LENGTH_LONG).show();
+            } else {
+                //if qr contains data
+                try {
+                    //converting the data to json
+                    JSONObject obj = new JSONObject(result.getContents());
+                    //setting values to textviews
+                    Log.d("EYY", obj.toString());
+                    textViewName.setText(obj.getString("name"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    //if control comes here
+                    //that means the encoded format not matches
+                    //in this case you can display whatever data is available on the qrcode
+                    //to a toast
+                    Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+                }
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
         }
     }
 }
